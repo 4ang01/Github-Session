9171edd7214cb1a5eea79144f3773b2f0a480d20
Geunyeop Park <geunyeop8015@gmail.com>
2024-04-18 20:29:35 +0900

    First commit

---
+++ Github Session22/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Github Session22
+//
+//  Created by 박근엽 on 4/18/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, Khaki")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
